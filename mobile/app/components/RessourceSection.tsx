ac246fd3bba39ee2ed73ed659058e6102013f9ae
Joseph MAUJEAN <joseph.maujean@viacesi.fr>
2025-04-01 00:18:42 +0200

    feat: Ajout de expo-router

--- mobile/app/components/RessourceSection.tsx
+++ mobile/app/components/RessourceSection.tsx
@@ -1,27 +1,27 @@
+// RessourceSection.tsx
 import React from 'react';
-import { View, Image, ScrollView, Text, StyleSheet } from 'react-native';
-import BlogCard from "./RessourceCard";
+import { View, ScrollView, StyleSheet } from 'react-native';
 import RessourceCard from "./RessourceCard";
 
-const BlogSection = () => {
-
-    // La team, faut modifier ici pour boucler sur les ressources de la base de données
+const RessourceSection = ({ itemCount }) => {
     const articles = [
-        { id: 1, title: "Articlo 1", image: require('../../assets/adaptive-icon.png'), description: 'Descirption courte d\'une ressource avec des infos... '},
-        { id: 2, title: "Article 2", image: require('../../assets/adaptive-icon.png'), description: 'Descirption courte d\'une ressource avec des infos... ' },
-        { id: 3, title: "Article 3", image: require('../../assets/adaptive-icon.png'), description: 'Descirption courte d\'une ressource avec des infos... ' },
-        { id: 4, title: "Article 3", image: require('../../assets/adaptive-icon.png'), description: 'Descirption courte d\'une ressource avec des infos... ' },
-        { id: 5, title: "Article 3", image: require('../../assets/adaptive-icon.png'), description: 'Descirption courte d\'une ressource avec des infos... ' },
-        { id: 6, title: "Article 3", image: require('../../assets/adaptive-icon.png'), description: 'Descirption courte d\'une ressource avec des infos... ' },
-        { id: 7, title: "Article 3", image: require('../../assets/adaptive-icon.png'), description: 'Descirption courte d\'une ressource avec des infos... ' },
-        { id: 8, title: "Article 3", image: require('../../assets/adaptive-icon.png'), description: 'Descirption courte d\'une ressource avec des infos... ' },
-        { id: 9, title: "Article 3", image: require('../../assets/adaptive-icon.png'), description: 'Descirption courte d\'une ressource avec des infos... ' },
+        { id: 1, title: "Article 1", image: require('../../assets/adaptive-icon.png'), description: 'Description courte d\'une ressource avec des infos...' },
+        { id: 2, title: "Article 2", image: require('../../assets/adaptive-icon.png'), description: 'Description courte d\'une ressource avec des infos...' },
+        { id: 3, title: "Article 3", image: require('../../assets/adaptive-icon.png'), description: 'Description courte d\'une ressource avec des infos...' },
+        { id: 4, title: "Article 4", image: require('../../assets/adaptive-icon.png'), description: 'Description courte d\'une ressource avec des infos...' },
+        { id: 5, title: "Article 5", image: require('../../assets/adaptive-icon.png'), description: 'Description courte d\'une ressource avec des infos...' },
+        { id: 6, title: "Article 6", image: require('../../assets/adaptive-icon.png'), description: 'Description courte d\'une ressource avec des infos...' },
+        { id: 7, title: "Article 7", image: require('../../assets/adaptive-icon.png'), description: 'Description courte d\'une ressource avec des infos...' },
+        { id: 8, title: "Article 8", image: require('../../assets/adaptive-icon.png'), description: 'Description courte d\'une ressource avec des infos...' },
+        { id: 9, title: "Article 9", image: require('../../assets/adaptive-icon.png'), description: 'Description courte d\'une ressource avec des infos...' },
     ];
+
+    const displayedArticles = articles.slice(0, itemCount);
 
     return (
         <View style={styles.blogContainer}>
             <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-                {articles.map(article => (
+                {displayedArticles.map(article => (
                     <View key={article.id} style={styles.articleCard}>
                         <RessourceCard
                             image={article.image}
@@ -40,24 +40,10 @@ const styles = StyleSheet.create({
         marginTop: 20,
         paddingLeft: 10,
     },
-    sectionTitle: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginBottom: 10,
-    },
     articleCard: {
         width: 150,
         marginRight: 70,
     },
-    articleImage: {
-        width: '100%',
-        height: 100,
-        borderRadius: 10,
-    },
-    articleTitle: {
-        textAlign: 'center',
-        marginTop: 5,
-    },
 });
 
-export default BlogSection;
+export default RessourceSection;
