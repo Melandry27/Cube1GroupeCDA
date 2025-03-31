fa6ef83dee9d6d1165089b55f2314842ebb20199
Mathis Ternon <mathis.ternon@devinfluence.fr>
2025-03-31 22:58:58 +0200

    feat: initialisation du projet

---
+++ back-office/src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 style={{ fontFamily: 'Marianne, sans-serif'}}>Back Office</h1>
+      <p style={{ fontFamily: 'Spectral, sans-serif'}}>Bienvenue dans le backoffice</p>
+    </>
+  )
+}
+
+export default App
