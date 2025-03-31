2716baa92cf95d031be8b2b7802d5570c603f55d
Joseph MAUJEAN <joseph.maujean@viacesi.fr>
2025-04-01 00:18:42 +0200

    feat: Ajout de expo-router

---
+++ mobile/app/(main)/_layout.tsx
@@ -0,0 +1,47 @@
+import { Tabs } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+
+export default function MainLayout() {
+    return (
+        <Tabs>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Accueil",
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome name="home" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="(ressources)"
+                options={{
+                    title: "Ressources",
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome6 name="book" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="(journaledebord)/index"
+                options={{
+                    title: "Journal de Bord",
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome6 name="chart-column" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="(main)/profile"
+                options={{
+                    title: "Profile",
+                    headerShown: false,
+                }}
+            />
+        </Tabs>
+    );
+}
