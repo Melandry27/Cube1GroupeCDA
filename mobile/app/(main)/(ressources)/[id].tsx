53c355cd2638fee286f5642233a794d7a0e42ca1
Joseph MAUJEAN <joseph.maujean@viacesi.fr>
2025-04-01 23:56:32 +0200

    feat: Ajout de la redirection vers une ressource

--- mobile/app/(main)/(ressources)/[id].tsx
+++ mobile/app/(main)/(ressources)/[id].tsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { useRouter } from 'expo-router';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
+import Title from "../../components/Title";
 
 export default function RessourceDetail() {
-    const { query } = useRouter();
-    const { id } = query;
+    const { id } = useLocalSearchParams();
+
+    // Récupérer les données de la ressource à partir de l'ID
+    // const ressource = getRessourceById(id);
 
     return (
-        <View>
-            <Text>Détail de la ressource {id}</Text>
+        <View style={styles.container}>
+            <Title size={"medium"}>Titre de la ressource</Title>
+            <Image></Image>
+            <Text style={styles.text}>ID: {id}</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+
+        padding: 20,
+    },
+    text: {
+        fontSize: 18,
+    },
+});
