c7ff7b01a77ed4b99ea631e10432cd0c3ee3d217
Mathis Ternon <mathis.ternon@devinfluence.fr>
2025-04-01 19:00:26 +0200

    Feat: Ajout de la connexion et de l'interface d'administration

---
+++ back-office/src/components/Header.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface HeaderProps {
+  handleLogout: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ handleLogout }) => {
+  return (
+    <header className="fr-header">
+      <div className="fr-container fr-py-2w fr-grid-row fr-grid-row--middle">
+        <img
+          src="/src/assets/Ministère_des_Solidarités_et_de_la_Santé.svg"
+          alt="Ministère"
+          className="fr-mr-2w"
+          style={{ height: 50 }}
+        />
+        <h1 className="fr-h4">Tableau de Bord de l'Administration</h1>
+        <button className="fr-btn fr-btn--secondary fr-ml-auto" onClick={handleLogout}>
+          Déconnexion
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
