6cea928b801bb9765a6dc77ab9bf069e61e69636
Joseph MAUJEAN <joseph.maujean@viacesi.fr>
2025-04-21 18:51:14 +0200

    feat: ajout de la couleur dans les categories

--- API/src/models/Category.ts
+++ API/src/models/Category.ts
@@ -1,20 +1,22 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 interface ICategory extends Document {
-  name: string;
-  description: string;
+    name: string;
+    description: string;
+    color?: string;
 }
 
 const CategorySchema = new Schema<ICategory>(
-  {
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        name: { type: String, required: true },
+        description: { type: String, required: true },
+        color: { type: String, required: false },
+    },
+    {
+        timestamps: true,
+    }
 );
 
 export { ICategory };
 
-export default mongoose.model<ICategory>("Statistic", CategorySchema);
+export default mongoose.model<ICategory>("Category", CategorySchema);
