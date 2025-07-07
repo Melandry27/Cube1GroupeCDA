b0ab0a94bf302135bb76ec90f9d1be9154c1e580
Melandry <msmelandry@hotmail.fr>
2025-07-07 18:17:27 +0200

    fix: simplify MongoDB URI configuration in db.ts

--- API/src/config/db.ts
+++ API/src/config/db.ts
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
 const MONGO_URI =
-  process.env.NODE_ENV === "test"
-    ? process.env.MONGO_URI_TEST || "mongodb://127.0.0.1:27017/testdb"
-    : process.env.MONGO_URI || "mongodb://127.0.0.1:27017/prod";
+  process.env.MONGO_URI ||
+  "mongodb://admin:admin@mongo/cube1groupecda?authSource=admin";
+// process.env.NODE_ENV === "test"
+//   ? process.env.MONGO_URI_TEST || "mongodb://127.0.0.1:27017/testdb"
+//   : process.env.MONGO_URI || "mongodb://127.0.0.1:27017/prod";
 
 const connectDB = async () => {
   try {
