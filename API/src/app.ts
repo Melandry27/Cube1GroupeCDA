6c0037702503811d5e1403003a4131e20a40f213
Melandry <msmelandry@hotmail.fr>
2025-07-07 18:10:17 +0200

    fix: specify dotenv config path for environment variables

--- API/src/app.ts
+++ API/src/app.ts
@@ -7,6 +7,7 @@ import connectDB from "./config/db";
 import Route from "./routes/index";
 import { setupSwagger } from "./swagger";
 
+dotenv.config({ path: "/app/.env" });
 dotenv.config();
 
 const app = express();
